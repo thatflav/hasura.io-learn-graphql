0aa703ca50732cc06b8aa114bbcd1128e2b64fbe
thatflav <flaviusbohaciuc@gmail.com>
2021-07-02 11:16:30 +0100

    fetch the existing todos from cache and write the updated todo list

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React from 'react';
+import { useMutation, gql } from '@apollo/client';
+import { GET_MY_TODOS } from './TodoPrivateList';
 
 const TodoItem = ({ index, todo }) => {
-  const removeTodo = e => {
+  const removeTodo = (e) => {
     e.preventDefault();
     e.stopPropagation();
   };
 
-  const toggleTodo = () => {};
+  const TOGGLE_TODO = gql`
+    mutation toggleTodo($id: Int!, $isCompleted: Boolean!) {
+      update_todos(
+        where: { id: { _eq: $id } }
+        _set: { is_completed: $isCompleted }
+      ) {
+        affected_rows
+      }
+    }
+  `;
+
+  const [toggleTodoMutation] = useMutation(TOGGLE_TODO);
+
+  const toggleTodo = () => {
+    toggleTodoMutation({
+      variables: { id: todo.id, isCompleted: !todo.is_completed },
+      optimisticResponse: true,
+      update: (cache) => {
+        const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+        const newTodos = existingTodos.todos.map((t) => {
+          if (t.id === todo.id) {
+            return { ...t, is_completed: !t.is_completed };
+          } else {
+            return t;
+          }
+        });
+        cache.writeQuery({
+          query: GET_MY_TODOS,
+          data: { todos: newTodos },
+        });
+      },
+    });
+  };
 
   return (
     <li>
@@ -22,7 +56,7 @@ const TodoItem = ({ index, todo }) => {
         </div>
       </div>
 
-      <div className={"labelContent" + (todo.is_completed ? " completed" : "")}>
+      <div className={'labelContent' + (todo.is_completed ? ' completed' : '')}>
         <div>{todo.title}</div>
       </div>
 
