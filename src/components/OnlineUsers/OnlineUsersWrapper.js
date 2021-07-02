15defa5f3acc899d735d30673f0c547d96e808ce
thatflav <flaviusbohaciuc@gmail.com>
2021-07-02 11:39:05 +0100

    add updateLastSeen mutation and run it every 20s to tell BE if user is online

--- src/components/OnlineUsers/OnlineUsersWrapper.js
+++ src/components/OnlineUsers/OnlineUsersWrapper.js
@@ -1,9 +1,39 @@
-import React from "react";
-
-import OnlineUser from "./OnlineUser";
+import React, { useEffect, useState } from 'react';
+import { useMutation, gql } from '@apollo/client';
+import OnlineUser from './OnlineUser';
 
 const OnlineUsersWrapper = () => {
-  const onlineUsers = [{ name: "someUser1" }, { name: "someUser2" }];
+  const [onlineIndicator, setOnlineIndicator] = useState(0);
+
+  useEffect(() => {
+    // Every 20s, run a mutation to tell the backend that you're online
+    updateLastSeen();
+    setOnlineIndicator(setInterval(() => updateLastSeen(), 20000));
+
+    return () => {
+      // Clean up
+      clearInterval(onlineIndicator);
+    };
+  }, []);
+
+  const UPDATE_LASTSEEN_MUTATION = gql`
+    mutation updateLastSeen($now: timestamptz!) {
+      update_users(where: {}, _set: { last_seen: $now }) {
+        affected_rows
+      }
+    }
+  `;
+
+  const [updateLastSeenMutation] = useMutation(UPDATE_LASTSEEN_MUTATION);
+
+  const updateLastSeen = () => {
+    // Use the apollo client to run a mutation to update the last_seen value
+    updateLastSeenMutation({
+      variables: { now: new Date().toISOString() },
+    });
+  };
+
+  const onlineUsers = [{ name: 'someUser1' }, { name: 'someUser2' }];
 
   const onlineUsersList = [];
   onlineUsers.forEach((user, index) => {
